bc45b519cfe0cd95f2ce9479abdc0de90520aad5
pinkienort <cantabilehisa@gmail.com>
2017-06-19 16:31:42 +0900

    ensure_dir, normalize

--- pyutils.py
+++ pyutils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import math
+import os
 
 def maximum_rectangle(l):
-
     if (l==0):
         return 0
     lim = math.ceil(math.sqrt(l)) + 1
@@ -17,4 +17,15 @@ def maximum_rectangle(l):
             min_leng = leng
             min_a, min_b = a, b
     return min_a, min_b
+
+
+def ensure_dir(path):
+    directory = os.path.dirname(path)
+    if directory == '':
+        return
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def normalize(x, x_max, x_min):
+    return (x - x_min) / (x_max - x_min)
 
